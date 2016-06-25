6c90cbe4b2f8dd582484bcaceae4d85af2dc65ae
songwenju <hgdswj@163.com>
2016-06-25 09:16:47 +0800

    Initial commit

---
+++ app/src/main/java/com/wjustudio/mobileplayer/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wjustudio.mobileplayer.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wjustudio.mobileplayer.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
